4d38bc6c475d30407eb91b61decaf05341a553e2
AnyiSigns <anyi.signs@gmail.com>
2026-05-30 23:35:39 +0800

    Update ai_parther_1.py

--- ai_parther_1.py
+++ ai_parther_1.py
@@ -54,7 +54,7 @@ def delects_session(seesion):
         os.remove(file_path)
         if st.session_state.current_session == seesion:
             st.session_state.messages = []
-            st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M")
         st.rerun()
 
 
@@ -95,7 +95,7 @@ if "system_name" not in st.session_state:
 if "nature" not in st.session_state:
     st.session_state.nature = "懵懂女性大学生，干啥啥不会，一问三不知"
 if "current_session" not in st.session_state:
-    st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M")
 
 # URL存储uuid（确保在调用任何依赖user_uuid的函数前设置）
 if "user_uuid" not in st.session_state:
@@ -124,7 +124,7 @@ with st.sidebar:
         if st.session_state.messages:
             converse()
             st.session_state.messages = []
-            st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            st.session_state.current_session = datetime.now().strftime("%Y-%m-%d_%H-%M")
             st.rerun()
 
     # 历史会话列表
